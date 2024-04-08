466cc71e655e88300c8fdd0e954f5dc0b037c57d
Evgenii Krasikov <krasikov.ev2@gmail.com>
2024-04-08 23:30:36 +0500

    -add property bot -send test message

---
+++ src/main/java/by/asckarugin/telegrambot/TelegramBot.java
@@ -0,0 +1,53 @@
+package by.asckarugin.telegrambot;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+@Component
+public class TelegramBot extends TelegramLongPollingBot {
+
+    @Value("${telegram.bot.name}")
+    private String name;
+
+    @Value("${telegram.bot.token}")
+    private String token;
+
+    @Override
+    public String getBotUsername() {
+        return name;
+    }
+
+    @Override
+    public String getBotToken() {
+        return token;
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if(update.hasMessage() && update.getMessage().hasText()){
+            long chatId = update.getMessage().getChatId();
+            String getFirstName = update.getMessage().getChat().getFirstName();
+
+            String greetings = "Привет "+getFirstName+" твой chatId: "+chatId;
+
+            sendMessage(chatId, greetings);
+        }
+
+    }
+
+    public void sendMessage(long chatId, String textToSend){
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setChatId(chatId);
+        sendMessage.setText(textToSend);
+
+        try{
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/main/java/by/asckarugin/telegrambot/TelegramBotInit.java
@@ -0,0 +1,24 @@
+package by.asckarugin.telegrambot;
+
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+@Component
+public class TelegramBotInit {
+
+    private final TelegramBot telegramBot;
+
+    public TelegramBotInit(TelegramBot telegramBot) {
+        this.telegramBot = telegramBot;
+    }
+
+    @EventListener({ContextRefreshedEvent.class})
+    public void init() throws TelegramApiException {
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+        telegramBotsApi.registerBot(telegramBot);
+    }
+}
