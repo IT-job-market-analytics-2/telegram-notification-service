e28bf918f93f069b0f1d9e89e1cd649cea1e8ee1
Evgenii Krasikov <krasikov.ev2@gmail.com>
2024-04-08 22:28:57 +0500

    create project

---
+++ src/main/java/by/asckarugin/TelegramNotificationServiceApplication.java
@@ -0,0 +1,11 @@
+package by.asckarugin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramNotificationServiceApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(TelegramNotificationServiceApplication.class, args);
+	}
+}
